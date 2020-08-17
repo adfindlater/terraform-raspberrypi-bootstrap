7cd35823469d55413dafe45ffd2565ffca604c10
Alex <alex@findlater.us>
2020-08-16 20:32:10 -0400

    update apt-get call

--- k8s_prep.sh
+++ k8s_prep.sh
@@ -42,7 +42,7 @@ cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
-sudo apt-get update && sudo apt-get install kubeadm=1.17.4-00 kubelet=1.17.4-00 kubectl=1.17.4-00 kubernetes-cni
+sudo apt-get update && sudo apt-get install -y kubeadm=1.17.4-00 kubelet=1.17.4-00 kubectl=1.17.4-00 kubernetes-cni
 
 # sudo apt update && sudo apt install -y kubelet=1.17.11-00 kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
